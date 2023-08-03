d844fa9f3ddcadda456dd0473a620e329de9926c
Ilhamokta74 <ilhamoktavian74@gmail.com>
2023-08-03 13:48:20 +0700

    clear branch

--- api/mentah/supabase.js
+++
@@ -1,8 +0,0 @@
-const {createClient} = require('@supabase/supabase-js')
-
-const SUPABASE_KEY = process.env.SERVICE_KEY
-const SUPABASE_URL = "https://pdtbytarvxlfrkgmfaqq.supabase.co"
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export default supabase;
